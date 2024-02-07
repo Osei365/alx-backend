9cdcf13e024be9947bed0491d3958eba118f25ed
Osei <williamsabodunrin@gmail.com>
2024-02-07 00:40:03 +0000

    babel intro

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""flask app."""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """config class."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """index view handler."""
+
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
